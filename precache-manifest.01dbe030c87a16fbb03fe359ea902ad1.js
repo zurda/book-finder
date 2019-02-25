7732c84da1b45e5a479c97f374c04b31c9d5facb
zurda <m.weizman@gmail.com>
2019-02-25 19:12:00 +0000

    Updates

---
+++ precache-manifest.01dbe030c87a16fbb03fe359ea902ad1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "8e4173ae76d365ec35fa",
+    "url": "/book-finder/static/js/main.8e4173ae.chunk.js"
+  },
+  {
+    "revision": "6c31e4adaa6efdbae152",
+    "url": "/book-finder/static/js/2.6c31e4ad.chunk.js"
+  },
+  {
+    "revision": "3c8d57c9c901eb2fb481a42e85f8cf05",
+    "url": "/book-finder/index.html"
+  }
+];
